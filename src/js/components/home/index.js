34f0dc52af03b65d3e898f7dc7ba5d02b5e57159
Matt Uttridge <matt@trazyinc.com>
2016-08-31 00:21:17 +0100

    Get a weather image in there

--- src/js/components/home/index.js
+++ src/js/components/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import { Link } from 'react-router'
+import weather from '../../../assets/images/weather.png';
 
 /**
  * Screen showing the user how to get a forecast.
@@ -14,6 +15,7 @@ export default class Home extends React.Component {
           <h1>Weather Forecast</h1>
         </Jumbotron>
         <p>Try one of our weather forecasts by clicking <Link to={'/London'}>here</Link></p>
+        <img src={weather} />
       </div>
     )
   }
