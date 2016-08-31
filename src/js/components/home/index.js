485d247d90f0c8e1e6047ccdea7364c5576624b4
Matt Uttridge <matt@trazyinc.com>
2016-08-31 09:38:48 +0100

    Styling of weather image

--- src/js/components/home/index.js
+++ src/js/components/home/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import { Link } from 'react-router'
+
+import { StyleSheet, css } from 'aphrodite';
 import weather from '../../../assets/images/weather.png';
+
+const styles = StyleSheet.create({
+    pic: {
+        height: 100,
+        width: 100
+    }
+});
 
 /**
  * Screen showing the user how to get a forecast.
@@ -14,8 +23,7 @@ export default class Home extends React.Component {
         <Jumbotron>
           <h1>Weather Forecast</h1>
         </Jumbotron>
-        <p>Try one of our weather forecasts by clicking <Link to={'/London'}>here</Link></p>
-        <img src={weather} />
+        <p><img className={css(styles.pic)} src={weather} /> Lets take a look at the weather in London by clicking <Link to={'/London'}>here</Link></p>
       </div>
     )
   }
