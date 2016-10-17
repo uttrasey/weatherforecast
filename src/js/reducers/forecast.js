509f6437972c23b7afc840efcc499063bf7367d4
Matt Uttridge <uttrasey@gmail.com>
2016-10-17 12:49:28 +0100

    use constants in the reducer too

--- src/js/reducers/forecast.js
+++ src/js/reducers/forecast.js
@@ -1,8 +1,11 @@
 import { groupForecastsByDay } from '../helpers/weather';
+import {
+    NEW_FORECAST
+} from '../constants/actionNames';
 
 const forecast = (state = {}, action) => {
     switch (action.type) {
-    case 'NEW_FORECAST':
+    case NEW_FORECAST:
         const newState = Object.assign({}, state);
         const groupedForecasts = groupForecastsByDay(action.forecast);
         newState[action.location] = groupedForecasts;
